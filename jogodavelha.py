f89c644bf44db30c058d30efe8dc72bcb1f3743f
nicole-m0 <nicolemendes.dev@gmail.com>
2026-04-05 15:01:36 -0300

    correções e melhorias

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,6 +1,16 @@
 # mudando algumas cores do código com a biblioteca colorama
 from colorama import Fore
 
+# testando opção de reiniciar o jogo
+def reiniciar():
+    while True:
+        opcao = input(Fore.YELLOW+ "\nDeseja jogar novamente? (y/n): ").lower()
+        if opcao == 'y':
+            return True
+        else:
+            print(Fore.LIGHTWHITE_EX+ "Jogo Encerrado!")
+            return False
+
 def interface():
     print("   0   1   2")
     print("0 [{}] [{}] [{}]".format(tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2]))
@@ -8,7 +18,7 @@ def interface():
     print("2 [{}] [{}] [{}]".format(tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2]))
 
 def validarVitoria(rodada):
-    global parar
+    global parar, tabuleiro
     # HORIZONTAL ---------------------------------------------------------------------------
     if(tabuleiro[0][0] == rodada and tabuleiro[0][1] == rodada and tabuleiro[0][2] == rodada):
         interface()
@@ -52,43 +62,48 @@ def validarVitoria(rodada):
         print(Fore.GREEN + "O {} Venceu!".format(rodada))
         parar = True
 
+while True:
 
-tabuleiro = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-parar = False
-rodada = "x"
-jogadas = 0
+    tabuleiro = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-while parar == False:
-    if jogadas == 9:
-        interface()
-        print(Fore.YELLOW + "Empate!")
-        parar = True
+    parar = False
+    rodada = "x"
+    jogadas = 0
 
-    interface()
-
-    linha = int(input(Fore.CYAN + "Digite a linha escolhida: "))
-    coluna = int(input(Fore.CYAN + "Digite a coluna escolhida: "))
-
-    if rodada == "x":
-        tabuleiro[linha][coluna] = "x"
-        validarVitoria(rodada)
-        jogadas += 1
-        rodada = "o"
-
-    elif rodada == "o":
-        tabuleiro[linha][coluna] = "o"
-        validarVitoria(rodada)
-        jogadas += 1
-        rodada = "x"
-
-# testando opção de reiniciar o jogo
-def reiniciar():
-    continuar = "y"
-    while continuar:
-        continuar = str(input("Deseja iniciar mais uma partida? y/n"))
-        if continuar:
+    while parar == False:
+        if jogadas == 9:
             interface()
-        else: print(Fore.LIGHTWHITE_EX + "Jogo Encerrado!")
+            print(Fore.YELLOW + "Deu Velha!")
+            break
 
+        interface()
 
+        linha = int(input(Fore.CYAN + "Digite a linha escolhida: "))
+        coluna = int(input(Fore.CYAN + "Digite a coluna escolhida: "))
+
+     # testando impedir entrada inválida:
+        if linha not in [0, 1, 2] or coluna not in [0, 1, 2]:
+            print("Ops,opção errada... tente novamente 0,1 ou 2 por favor!")
+            continue
+
+    # testando impedir jogada em posição ocupada:
+        if tabuleiro [linha][coluna] != " ":
+            print(Fore.LIGHTRED_EX+ "Essa posição já está ocupada...tente outra por favor!")
+            continue
+
+        if rodada == "x":
+            tabuleiro[linha][coluna] = "x"
+            validarVitoria(rodada)
+            jogadas += 1
+            rodada = "o"
+
+        elif rodada == "o":
+            tabuleiro[linha][coluna] = "o"
+            validarVitoria(rodada)
+            jogadas += 1
+            rodada = "x"
+
+# se usuário clicar em "y" o jogo reinicia:
+    if reiniciar():
+        print(Fore.LIGHTWHITE_EX+"Reiniciando...")
